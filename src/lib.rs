612f501976576c2aff844b661dae45218f92b935
Zheng Hao Tan <hi@zhenghaotan.com>
2023-07-14 23:56:02 +0800

    add main and lib func

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let file_contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{file_contents}");
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,6 @@
+use minigrep::Config;
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
-        Ok(Config {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-        })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let file_contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{file_contents}");
-
-    Ok(())
-}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -39,7 +13,7 @@ fn main() {
     print!("Searching for {}", config.query);
     println!(", in file: {}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
