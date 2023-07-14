ab52a84bfa49f97e86170da4a4d1f980c3ef0b8a
Zheng Hao Tan <hi@zhenghaotan.com>
2023-07-14 22:06:20 +0800

    add parser for file content

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    print!("Searching for {query}");
+    println!(", in file: {file_path}");
+
+    let file_read_error_msg = "Should be able to read the file";
+    let file_contents = fs::read_to_string(file_path).expect(file_read_error_msg);
+    
+    println!("With text:\n{file_contents}");
 }
