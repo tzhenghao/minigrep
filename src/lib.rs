f6fbc9470841872af01c2678ffc4f581dee5e1a0
Zheng Hao Tan <hi@zhenghaotan.com>
2023-07-15 00:15:36 +0800

    fix unit test

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut result: Vec<&'a str> = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
 }
 
 #[cfg(test)]
@@ -36,9 +42,9 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-        Rust:
-        safe, fast, productive.
-        Pick three.";
+Rust:
+safe, fast, productive.
+Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
