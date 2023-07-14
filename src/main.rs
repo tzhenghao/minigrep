0391c4b34221a9314f1eaf6bb663b47a36abb7e4
Zheng Hao Tan <hi@zhenghaotan.com>
2023-07-14 23:17:11 +0800

    add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
     file_path: String,
 }
 
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
     print!("Searching for {}", config.query);
     println!(", in file: {}", config.file_path);
 
@@ -17,14 +33,4 @@ fn main() {
     let file_contents = fs::read_to_string(config.file_path).expect(file_read_error_msg);
 
     println!("With text:\n{file_contents}");
-}
-
-fn parse_config(args: &[String]) -> Config {
-    if args.len() < 3 {
-        panic!("Not enough arguments!");
-    }
-    Config {
-        query: args[1].clone(),
-        file_path: args[2].clone(),
-    }
 }
