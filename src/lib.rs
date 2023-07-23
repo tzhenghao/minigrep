2e31d0cf06270f4c8bcf5705d5cbf34d73ecc39a
Zheng Hao Tan <hi@zhenghaotan.com>
2023-07-23 15:35:30 +0800

    update build interface

--- src/lib.rs
+++ src/lib.rs
@@ -9,15 +9,27 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        // if args.len() < 3 {
+        //     return Err("Not enough arguments!");
+        // }
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
         Ok(Config {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
+            query,
+            file_path,
             ignore_case,
         })
     }
